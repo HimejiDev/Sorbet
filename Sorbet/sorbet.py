f75f5aa97ec71a304e9cf27696a70b4dcab899f1
dc: @himeji <tobin.post@gmail.com>
2023-11-17 16:15:40 +0100

    ✨ Basic Sorbet class and calling done.

---
+++ Sorbet/sorbet.py
@@ -0,0 +1,52 @@
+from color import *
+
+DEFAULT_PREFIX = "🍨 | "
+
+# 🍨 | [object name]: [object Object]
+# 🍨 | [file].py:[line] > [function] | [time] ; [date]
+
+
+class Sorbet:
+    """The Sorbet class."""
+
+    def __init__(self, prefix=DEFAULT_PREFIX, output=False, includeContext=False):
+        """Initializes the Sorbet class.
+
+        :param prefix: The prefix to use for the logger.
+        :param output: Whether to output to the console.
+        :param includeContext: Whether to include the context in the output.
+
+        :type prefix: str
+        :type output: bool
+        :type includeContext: bool
+
+        :return: None
+        """
+        self.enabled = True
+        self.prefix = prefix
+        self.output = output
+        self.includeContext = includeContext
+
+    def __call__(self, *args):
+        """Called when the class is called.
+
+        :param args: The arguments to pass to the class.
+
+        :type args: Any
+
+        :return: Any | None
+        """
+        print(
+            f"{color.bright_black(self.prefix)}Hi! I'm Sorbet, a logger made by Himeji."
+        )
+
+        if not args:  # sb()
+            returns = None
+        elif len(args) == 1:  # sb(1)
+            returns = args[0]
+        else:  # sb(1, 2, 3).
+            returns = args
+        return returns
+
+
+sb = Sorbet()
