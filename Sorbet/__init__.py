118e87e776044b91506802508d035d2b5883f4b1
dc: @himeji <tobin.post@gmail.com>
2023-11-17 16:16:01 +0100

    ✨ Added some variables

---
+++ Sorbet/__init__.py
@@ -0,0 +1,11 @@
+from sorbet import *
+
+# variables
+__version__ = "0.0.1"
+__author__ = "Himeji"
+__license__ = "MIT"
+__title__ = "Sorbet"
+__description__ = "A logger made by Himeji."
+__url__ = "https://himeji.dev/sorbet"
+__github__ = "https://github.com/HimejiDev/Sorbet/"
+__docs__ = "https://himeji.dev/sorbet/docs"
